34b9cc14809e160e317f098d0ac74eebabbe95cb
Nayem <nayem.alam@mail.mcgill.ca>
2020-04-08 16:13:44 -0400

    removed abstract images

--- src/components/navigationbar/sidedrawer/mainmenu/mainavatar/MainAvatar.jsx
+++ src/components/navigationbar/sidedrawer/mainmenu/mainavatar/MainAvatar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import personalPic from '../../../../../images/avatar.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,7 +10,9 @@ class MainAvatar extends Component {
     render () {
         return (
             <div className='mainavatar'>
-                <Avatar alt="Nayem Alam" src={personalPic} style={{width: '130px', height: '130px', margin: 'auto'}}/>
+                <Link to='/'>
+                    <Avatar alt="Nayem Alam" src={personalPic} style={{width: '130px', height: '130px', margin: 'auto'}}/>
+                </Link>
                 <h2>Nayem Alam</h2>
                 <p style={{marginTop: '-10px'}}><FontAwesomeIcon icon={faMapMarkerAlt} size='sm'/> Montréal, QC</p>
             </div>
