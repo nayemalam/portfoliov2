2fa8b3f261ed98bbd1c848d20b454111439f0737
Nayem <nayem.alam@mail.mcgill.ca>
2020-04-08 15:22:44 -0400

    added dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { render, hydrate } from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById('root')
-
-if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-} else {
-    render(<App />, rootElement);
-}
-
-serviceWorker.register();
+render(
+    <App />,
+    document.getElementById('root'));
+serviceWorker.unregister();
