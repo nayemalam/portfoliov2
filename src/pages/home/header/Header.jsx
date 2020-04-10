64f8e018acd5a5c925e2fbd8115a0ed460ef6ea5
Nayem <nayem.alam@mail.mcgill.ca>
2020-04-10 17:23:21 -0400

    updated homepage looks

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -4,8 +4,8 @@ class Header extends Component {
   render() {
     return (
       <div className='header'>
-        <h1 className='customHeader centerText' style={{fontSize: '45px', color: '#07bbf2'}}>Hi, I'm Nayem.</h1>
-        <p className='customHeader centerText justifyText' style={{margin: '0 50px 0 50px', color: '#8cea95'}}>
+        <h1 className='customHeader centerText' style={{fontSize: '45px', color: 'rgb(195, 232, 141)'}}>Hi, I'm Nayem.</h1>
+        <p className='customHeader centerText justifyText' style={{margin: '0 50px 0 50px', color: 'rgb(199, 146, 234)'}}>
         Slam poet, designer, developer, engineer. These are all titles I've been given. I like 
         to think of myself as a learner, from cooking to up and coming programming frameworks. 
         Continue reading and see what title you want to give me.
--- src/pages/home/socials/Socials.jsx
+++ src/pages/home/socials/Socials.jsx
@@ -9,13 +9,13 @@ class Socials extends Component {
             <div className='socials'>
                  <Grid container spacing={4}>
                     <Grid item xs={4} sm={4} style={{marginRight: '-20px'}}>
-                    <FontAwesomeIcon icon={faGithub} size='2x' style={{marginRight: '25px', color: '#d5d6db'}}/>
-                    <FontAwesomeIcon icon={faLinkedinIn} size='2x' style={{marginRight: '25px', color: '#d5d6db'}}/>
-                    <FontAwesomeIcon icon={faInstagram} size='2x' style={{marginRight: '25px', color: '#d5d6db'}}/>
-                    <FontAwesomeIcon icon={faMedium} size='2x' style={{marginRight: '15px', color: '#d5d6db'}}/>
+                    <FontAwesomeIcon icon={faGithub} size='2x' style={{marginRight: '25px', color: '#bfc7d5'}}/>
+                    <FontAwesomeIcon icon={faLinkedinIn} size='2x' style={{marginRight: '25px', color: '#bfc7d5'}}/>
+                    <FontAwesomeIcon icon={faInstagram} size='2x' style={{marginRight: '25px', color: '#bfc7d5'}}/>
+                    <FontAwesomeIcon icon={faMedium} size='2x' style={{marginRight: '15px', color: '#bfc7d5'}}/>
                     </Grid>
                     <Grid item xs={4} sm={4}><hr className='customHr'/></Grid>
-                    <Grid item xs={4} sm={4} className='customHeader' style={{color: '#d5d6db'}}>My Social Profiles</Grid>
+                    <Grid item xs={4} sm={4} className='customHeader' style={{color: '#bfc7d5'}}>My Social Profiles</Grid>
                 </Grid>
             </div>
         )
