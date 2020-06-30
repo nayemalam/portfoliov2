107a0368145fe1761d7a7d817595a1542888ac66
Nayem <nayem.alam@mail.mcgill.ca>
2020-06-30 17:18:17 -0400

    updated meta images

--- src/components/metatags/MetaTags.jsx
+++ src/components/metatags/MetaTags.jsx
@@ -13,18 +13,19 @@ class MetaTags extends Component {
                 {/* Open Graph / Facebook */}
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://nayemalam.com/" />
+                <meta property="article:author" content="Nayem Alam" />
                 <meta property="og:title" content={this.props.title} />
                 <meta property="og:description" content="Nayem Alam is a 23 y/o tech enthusiast, slam poet, engineer, two-time TEDx speaker, WEday speaker, one of Canada's Top 20 Under 20, and a Montreal Global Shaper." />
-                <meta property="og:image" content="https://nayemalam.com/static/headshot-d4708e9a3fbccec55710a79aa3b756a5.jpg" />
+                <meta property="og:image" content="https://res.cloudinary.com/nayemalam/image/upload/v1593551252/portfolio/images/meta-cards/facebook-card.png" />
 
                 {/* Twitter */}
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://nayemalam.com/" />
-                <meta name="twitter:creator" content="@nayem_wizdom" />
+                <meta property="twitter:creator" content="@nayem_wizdom" />
                 <meta property="twitter:title" content={this.props.title} />
                 <meta property="twitter:description" content="Nayem Alam is a 23 y/o tech enthusiast, slam poet, engineer, two-time TEDx speaker, WEday speaker, one of Canada's Top 20 Under 20, and a Montreal Global Shaper." />
                 {/* Twitter summary card with large image must be at least 280x150px */}
-                <meta property="twitter:image" content="https://nayemalam.com/static/headshot-d4708e9a3fbccec55710a79aa3b756a5.jpg" />                
+                <meta property="twitter:image" content="https://res.cloudinary.com/nayemalam/image/upload/v1593551835/portfolio/images/meta-cards/twitter-card.png" />                
             </Helmet>
         )
     }
