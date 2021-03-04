4989917f1789a9b734c5062b233a703dbcd075d0
nayemalam <nayem.alam@mail.mcgill.ca>
2021-03-04 11:45:54 -0500

    feat: removed backend folder

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b52738e2bdf58587cc9760c1717e1aab'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '1df19b11-8766-4317-b443-32617348d90a'
-};
