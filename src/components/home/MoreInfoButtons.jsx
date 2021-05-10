1b7c5945218f2ec46a999d5ac95e518dc7aa4cf6
nayemalam <nayem.alam@mail.mcgill.ca>
2021-05-10 18:11:06 -0400

    feat: updated resume

--- src/components/home/MoreInfoButtons.jsx
+++ src/components/home/MoreInfoButtons.jsx
@@ -1,24 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 import { Link } from 'gatsby';
-import resume from '../../images/Nayem_Resume_2020.pdf';
+import resume from '../../images/Nayem_Resume_2021.pdf';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFingerprint, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
 class MoreInfoButtons extends Component {
-    render() {
-        
-        return (
-            <div className='moreinfobuttons'>
-                <Link to='/about' style={{textDecoration: 'none'}}>
-                    <Button className='btn mui-design'>READ MORE &nbsp; <FontAwesomeIcon icon={faFingerprint} width='16'/></Button>
-                </Link>
-                <a href={resume} target="_blank" rel='noopener noreferrer' style={{textDecoration: 'none'}}>
-                    <Button className='btn mui-design'>VIEW RESUME &nbsp; <FontAwesomeIcon icon={faFilePdf} width='16'/></Button>
-                </a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="moreinfobuttons">
+        <Link to="/about" style={{ textDecoration: 'none' }}>
+          <Button className="btn mui-design">
+            READ MORE &nbsp; <FontAwesomeIcon icon={faFingerprint} width="16" />
+          </Button>
+        </Link>
+        <a
+          href={resume}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: 'none' }}
+        >
+          <Button className="btn mui-design">
+            VIEW RESUME &nbsp; <FontAwesomeIcon icon={faFilePdf} width="16" />
+          </Button>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default MoreInfoButtons;
